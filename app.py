e6e94809b825fb041151cbd78a4417fe2d402a3e
kader9699 <kaderaboubacarslm@gmail.com>
2024-10-19 20:01:20 +0200

    second commit

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
-from flask import Flask, abort,render_template, session,request, redirect, url_for,jsonify,Blueprint
+from flask import Flask, abort,render_template, flash,session,request, redirect, url_for,jsonify,Blueprint
 import os
+from model import FileManager
+from werkzeug.utils import secure_filename
+
 
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = os.path.join(os.getcwd(), 'Fichier')
+app.config['ALLOWED_EXTENSIONS'] = {'xls', 'xlsx', 'csv'}
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16 MB
+app.secret_key = "t0pS3cr3t!2024" 
 
+def allowed_file(filename):
+    """
+    Vérifie si le fichier a une extension autorisée.
+    """
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
         
 # Api pour afficher la page index
 @app.route("/", methods=["POST","GET"])
@@ -14,13 +26,50 @@ def index():
 # Api pour gerer les fichier
 @app.route("/fichier", methods=["POST","GET"])
 def fichier():
+    if request.method == "POST":
+        if 'file' not in request.files:
+            flash('Aucun fichier sélectionné')
+            return redirect(request.url)
+
+        file = request.files['file']
+        
+        if file.filename == '':
+            flash('Aucun fichier sélectionné')
+            return redirect(request.url)
+
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            upload_folder = app.config['UPLOAD_FOLDER']
+            if not os.path.exists(upload_folder):
+                os.makedirs(upload_folder)
+            file_path = os.path.join(upload_folder, filename)
+            file.save(file_path)
+            # Garde le fichier dans la session
+            session['file_path'] = file_path
+            file_manager = FileManager(file_path)
+            file_manager.load_data()
+            data = file_manager.Extraction_data_Phylum('Bacteroidetes')   
+            return redirect(request.url)
+
     return render_template("fichier.html")
 
 
 # Api pour gerer les types de nutrition 
-@app.route("/phylum", methods=["POST","GET"])
-def phylum():
-    return render_template("phylum.html")
+@app.route("/analyse", methods=["POST","GET"])
+def analyse():
+    if request.method == "POST":
+        file_manager = FileManager(session['file_path'])
+        file_manager.load_data()
+        phylum = request.form['phylum']  
+        bacterie = request.form['bacterie']
+        nutriment = request.form['nutriment']
+        type = request.form['type']
+        print(phylum,nutriment,bacterie,type)
+        data = file_manager.Get_Data(bacterie,phylum,nutriment,type)
+        # Convertir le DataFrame en une liste de dictionnaires
+        data_dict = data.to_dict(orient='records')
+        return render_template("analyse.html",data = data_dict)
+    return render_template("analyse.html")
     
     
 if __name__ == "__main__":
---
+++ model.py
@@ -0,0 +1,137 @@
+import os
+import pandas as pd
+import re
+
+class FileManager:
+    def __init__(self, file_path):
+        self.data = None
+        self.path = file_path
+    def load_data(self):
+        """
+        Charge les données depuis le fichier CSV ou Excel dans un DataFrame.
+        """
+        if self.path.endswith('.csv'):
+            self.data = pd.read_csv(self.path,sep=';')
+        elif self.path.endswith(('.xls', '.xlsx')):
+            self.data = pd.read_csv(self.path,sep=';')
+    # Fonction permettant de renvoyer toute les donnees dans le fichier
+    def Extraction_Tout(self):
+        if self.data is not None:
+            return self.data
+    
+    # Extraction de sous données en fonction de bacterie
+    def Extraction_data_Bacterie(self,name):
+        return self.data[self.data['Souche Name'] == name]
+                
+    
+    # Extraction de sous données en fonction de phylum            
+    def Extraction_data_Phylum(self, name):
+        if self.data is not None:
+            return self.data[self.data['phylum'] == name]
+        else:
+            raise ValueError("Les données ne sont pas chargées. Appelez d'abord load_data().")
+    
+    # Extraction de sous données en fonction de nutriment 
+    def Extraction_data_Nutriment(self,name):
+        return self.data[self.data['ImpExp'].str.contains(re.escape(name))]
+    
+    # Extraction de sous données en fonction de d'import ou export 
+    def Extraction_data_Importe_Exporte(self,terme):
+        return self.data[self.data['ImpExp'].str.endswith(terme)]
+    
+    # Extraction de sous données en fonction de bacterie et phylum
+    def Extraction_data_Bacterie_Phylum(self,bacterie,phylum):
+        mask = ((self.data['Souche Name'] == bacterie) & (self.data['phylum'] == phylum))
+        return self.data[mask]
+    
+    # Extraction de sous données en fonction de bacterie et nutriment
+    def Extraction_data_Bacterie_Nutriment(self,bacterie,nutriment):
+        mask = ((self.data['Souche Name'] == bacterie) & (self.data['ImpExp'].str.contains(re.escape(nutriment))))
+        return self.data[mask]
+    
+    # Extraction de sous données en fonction de bacterie et import ou export
+    def Extraction_data_Bacterie_Importe_Exporte(self,bacterie,terme):
+        mask = ((self.data['Souche Name'] == bacterie) & (self.data['ImpExp'].str.endswith(terme)))
+        return self.data[mask]
+    
+    # Extraction de sous données en fonction de phylum et nutriment
+    def Extraction_data_Phylum_Nutriment(self,phylum,nutriment):
+        mask = ((self.data['phylum'] == phylum) & (self.data['ImpExp'].str.contains(re.escape(nutriment))))
+        return self.data[mask]
+            
+    # Extraction de sous données en fonction de phylum et import ou export
+    def Extraction_data_Phylum_Importe_Exporte(self,phylum,terme):
+        mask = ((self.data['phylum'] == phylum) & (self.data['ImpExp'].str.endswith(terme)))
+        return self.data[mask]
+    
+    # Extraction de sous données en fonction de  nutriment et import ou expot
+    def Extraction_data_Nutriment_Importe_Exporte(self,nutriment,terme):
+        mask = ((self.data['ImpExp'].str.contains(re.escape(nutriment))) & (self.data['ImpExp'].str.endswith(terme)))
+        return self.data[mask]
+    
+    # Extraction de sous données en fonction de bacterie, phylum et nutriment
+    def Extraction_data_Bacterie_Phylum_Nutriment(self,bacterie,phylum,nutriment):
+        mask = ((self.data['Souche Name'] == bacterie) & (self.data['phylum'] == phylum) & (self.data['ImpExp'].str.contains(re.escape(nutriment))))
+        return self.data[mask]
+    
+    # Extraction de sous données en fonction de bacterie, phylum et import ou export
+    def Extraction_data_Bacterie_Phylum_Importe_Exporte(self,bacterie,phylum,terme):
+        mask = ((self.data['Souche Name'] == bacterie) & (self.data['phylum'] == phylum) & (self.data['ImpExp'].str.endswith(terme)))
+        return self.data[mask]
+    
+    # Extraction de sous données en fonction de bacterie, nutriment et import ou expot
+    def Extraction_data_Bacterie_Nutriment_Importe_Exporte(self,bacterie,nutriment,terme):
+        mask = ((self.data['Souche Name'] == bacterie) & (self.data['ImpExp'].str.contains(re.escape(nutriment))) & (self.data['ImpExp'].str.endswith(terme)))
+        return self.data[mask]
+    
+    # Extraction de sous données en fonction de phylum, nutriment et import ou export
+    def Extraction_data_Phylum_Nutriment_Importe_Exporte(self,phylum,nutriment,terme):
+        mask = ((self.data['phylum'] == phylum) & (self.data['ImpExp'].str.contains(re.escape(nutriment))) & (self.data['ImpExp'].str.endswith(terme)))
+        return self.data[mask]
+    
+    # Extraction de sous données en fonction de bacterie, nutriment et import ou export
+    def Extraction_data_Bacterie_Phylum_Nutriment_Importe_Exporte(self,bacterie,phylum,nutriment,terme):
+        mask = ((self.data['Souche Name'] == bacterie) & (self.data['phylum'] == phylum) & (self.data['ImpExp'].str.contains(re.escape(nutriment))) & (self.data['ImpExp'].str.endswith(terme)))
+        return self.data[mask]
+            
+    # Extraction de sous données en fonction de bacterie, phylum, nutriment et import ou expot
+    def Extraction_data_Bacterie_Phylum_Nutriment_Importe_Exporte(self,bacterie,phylum,nutriment,terme):
+        mask = ((self.data['Souche Name'] == bacterie) & (self.data['phylum'] == phylum) & (self.data['ImpExp'].str.contains(re.escape(nutriment))) & (self.data['ImpExp'].str.endswith(terme))) 
+        return self.data[mask]
+
+    # Fonction permettant d'obtenir les donnee en fonction de requete de l'utilisateur
+    def Get_Data(self,bacterie,phylum,nutriment,type):
+        if phylum == 'Tout' and bacterie == 'Tout' and nutriment == 'Tout' and type == 'Tout':
+            data = self.Extraction_Tout()
+        elif phylum == 'Tout' and bacterie != 'Tout' and nutriment == 'Tout' and type == 'Tout':
+            data = self.Extraction_data_Bacterie(bacterie)
+        elif phylum != 'Tout' and bacterie == 'Tout' and nutriment == 'Tout' and type == 'Tout':
+            data = self.Extraction_data_Phylum(phylum) 
+        elif phylum == 'Tout' and bacterie == 'Tout' and nutriment != 'Tout' and type == 'Tout':
+            data = self.Extraction_data_Nutriment(nutriment) 
+        elif phylum == 'Tout' and bacterie == 'Tout' and nutriment == 'Tout' and type != 'Tout':
+            data = self.Extraction_data_Importe_Exporte(type) 
+        elif phylum != 'Tout' and bacterie != 'Tout' and nutriment == 'Tout' and type == 'Tout':
+            data = self.Extraction_data_Bacterie_Phylum(bacterie,phylum)
+        elif phylum != 'Tout' and bacterie == 'Tout' and nutriment != 'Tout' and type == 'Tout':
+            data = self.Extraction_data_Bacterie_Nutriment(bacterie,nutriment)
+        elif phylum != 'Tout' and bacterie == 'Tout' and nutriment == 'Tout' and type != 'Tout':
+            data = self.Extraction_data_Bacterie_Importe_Exporte(bacterie,type)  
+        elif phylum != 'Tout' and bacterie == 'Tout' and nutriment != 'Tout' and type == 'Tout':
+            data = self.Extraction_data_Phylum_Nutriment(phylum,nutriment) 
+        elif phylum != 'Tout' and bacterie == 'Tout' and nutriment == 'Tout' and type != 'Tout':
+            data = self.Extraction_data_Phylum_Importe_Exporte(phylum,type) 
+        elif phylum == 'Tout' and bacterie == 'Tout' and nutriment != 'Tout' and type != 'Tout':
+            data = self.Extraction_data_Nutriment_Importe_Exporte(nutriment,type) 
+        elif phylum != 'Tout' and bacterie != 'Tout' and nutriment != 'Tout' and type == 'Tout':
+            data = self.Extraction_data_Bacterie_Phylum_Nutriment(bacterie,phylum,nutriment)
+        elif phylum == 'Tout' and bacterie != 'Tout' and nutriment != 'Tout' and type != 'Tout':
+            data = self.Extraction_data_Bacterie_Nutriment_Importe_Exporte(bacterie,nutriment,type)
+        elif phylum != 'Tout' and bacterie == 'Tout' and nutriment != 'Tout' and type != 'Tout':
+            data = self.Extraction_data_Phylum_Nutriment_Importe_Exporte(phylum,nutriment,type)
+        elif phylum == 'Tout' and bacterie != 'Tout' and nutriment != 'Tout' and type != 'Tout':
+            data = self.Extraction_data_Bacterie_Nutriment_Importe_Exporte(bacterie,nutriment,type) 
+        elif phylum != 'Tout' and bacterie != 'Tout' and nutriment != 'Tout' and type != 'Tout':
+            data = self.Extraction_data_Bacterie_Phylum_Nutriment_Importe_Exporte(bacterie,phylum,nutriment,type) 
+        return data  
+          
